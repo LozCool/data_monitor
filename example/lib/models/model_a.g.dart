4aa737a8e2077ac0e3059ecf6126332b72f4407a
Loz Groves <lgroves@nqcomputing.com>
2026-06-10 17:07:19 +0300

    added example/example.md
    Added lots of other code documentation.

---
+++ example/lib/models/model_a.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'model_a.dart';
+
+// **************************************************************************
+// DataMonitorGenerator
+// **************************************************************************
+
+mixin _$ModelAData on ModelAData {
+  @override
+  set testInteger(int value) {
+    notifyListeners();
+
+    super.testInteger = value;
+  }
+
+  @override
+  set testString(String value) {
+    notifyListeners();
+
+    super.testString = value;
+  }
+
+  @override
+  set list(List<String> value) {
+    notifyListeners();
+
+    super.list = value;
+  }
+}
